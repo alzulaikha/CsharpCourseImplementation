ebf6b24eb657f802ab116b1186b7deb4f1383bb0
alzulaikha <alzulaikha1992@gmail.com>
2026-05-17 13:59:31 +0400

    Add project files.

---
+++ ClinicManagmentSystem/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ClinicManagmentSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //rejon 1 : system storage ( variables )
+            // store banck account information
+            string P1Name = "";
+            string P1Number = "";    
+            int P1Age = 0;
+            string P1Phone = "";
+            bool P1Active = false;
+           
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
