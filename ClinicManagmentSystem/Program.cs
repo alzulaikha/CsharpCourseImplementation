00b62bcf2100d3fad96aaac2db7b975dea22b448
alzulaikha <alzulaikha1992@gmail.com>
2026-05-17 14:12:47 +0400

    implementation main menu

--- ClinicManagmentSystem/Program.cs
+++ ClinicManagmentSystem/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ClinicManagmentSystem
+﻿using Microsoft.Win32;
+
+namespace ClinicManagmentSystem
 {
     internal class Program
     {
@@ -8,21 +10,39 @@
             //rejon 1 : system storage ( variables )
             // store banck account information
             string P1Name = "";
-            string P1Number = "";    
+            string P1Number = "";
             int P1Age = 0;
             string P1Phone = "";
             bool P1Active = false;
-           
 
 
+            //rejon 2 : system processing ( menu functions )
+            // print main menu
 
+
+            bool exit = false;
+            while (exit == false)
+
+            {
+
+                Console.Clear();
+
+                Console.WriteLine("CLINIC MANAGEMENT SYSTEM ");
+                Console.WriteLine("1.Registeration Patient ");
+                Console.WriteLine("2.View Patients");
+                Console.WriteLine("3.Edit Patient");
+                Console.WriteLine("Age Category");
+                Console.WriteLine("0. Exit");
+
+                Console.WriteLine("Select your option: ");
+                int option = int.Parse(Console.ReadLine());
+            }
+
+
+            }
         }
     }
-}
-
-
-
-
+    
 
 
 
