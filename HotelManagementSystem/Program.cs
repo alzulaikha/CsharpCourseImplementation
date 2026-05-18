13cf944a03117b220216fa89f08ee5e66de182e4
alzulaikha <alzulaikha1992@gmail.com>
2026-05-18 15:19:57 +0400

    Added variable

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace HotelManagementSystem
+﻿using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HotelManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            string guestName = "";
+            string guestPhone = "";
+            string roomNumber = "";
+            string roomType = "";
+            double nightlyRate = 0;
+            string checkInDate = "";
+            string checkOutDate = "";
+            int numberOfNights = 0;
+            string roomNotes = "";
+            double discountPercentage= 0;
+            double loyaltyPoints = 0;
+            bool registeredGuest = false;
+            bool checkedinGuest = false;
+
+
+
+
+
         }
     }
 }
