763910ca108403dc9c2cb2c398cbd6dce66611db
alzulaikha <alzulaikha1992@gmail.com>
2026-06-02 08:19:43 +0400

    Add main menu function

---
+++ TS_DS_01/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TS_DS_01
+{
+    internal class Program
+    {
+        //Main menu function 
+        public static void mainMenu()
+        {
+            Console.WriteLine("== Arrays Practice ==");
+            Console.WriteLine("1. Temperature Log");
+            Console.WriteLine("2. Student Score Board");
+            Console.WriteLine("3. Product Price Finder");
+            Console.WriteLine("4. Race Finish Times");
+            Console.WriteLine("5. Classroom Grade Report");
+            Console.WriteLine("6. Warehouse Inventory Check");
+            Console.WriteLine("7. Library book Shelf Scanner");
+            Console.WriteLine("8. Sales Performance Analyzer");
+            Console.WriteLine("9. Flight Seat Allocation Display");
+            Console.WriteLine("10. Hospital Patient Priority Queue");
+            Console.WriteLine("0. Exit");
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
